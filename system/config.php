31efdc53eff18a5d9cc47134e8269e5d2596087b
Vladimir Fedorov <fedorovvs@gmail.com>
2012-01-09 03:27:59 +0600

    site.cfg format changed

--- system/config.php
+++ system/config.php
@@ -1,50 +1,21 @@
 <?php
 
-define('ROOTDIR', $_SERVER['DOCUMENT_ROOT'].'/');
-
-$config = Config::read();
-
-// Paths //////////////////////////////////////////////////////////////////////
-
-// web path
-//define('ROOT', '/');
-
 // Root path
 define('ROOTDIR', $_SERVER['DOCUMENT_ROOT'].'/');
 
-
-// Database settings //////////////////////////////////////////////////////////
-
-// Databasse host
-//define('DBHOST', 'localhost');
-
-// Database name
-//define('DBNAME', 'cuprum');
-
-// Database user name
-//define('DBUSER', 'cuprum_dba');
-
-// Password for the database
-//define('DBPWD', 'T#tra_ah7yuw&b2=');
-
-
-// site address
-//define('SITEADDRESS', 'http://cuprum.vladimirfedorov.net');
+// Global config
+$config = Config::read();
 
 // system core file
 define('CORE', ROOTDIR.'/system/core.php');
-
-// Theme to use
-//define('THEME', 'default');
 
 // Template to use when a .php file is shown 
 // (default is 'system', used basically for system files)
 define('FILETEMPLATE','system');
 
-// Misc settings //////////////////////////////////////////////////////////////
 
 // Maimum number of iterations for template/variable/plugin processor 
-// 1000 is more than enough 
+// In most cases 100 is OK and 1000 is more than enough 
 define ('MAXITERATIONS', 100);
 
 // Remove or not plugins, variables and templates that was not found
@@ -52,7 +23,7 @@ define ('REMOVENONEXISTENT', true);
 
 class Config {
 	function read() {
-		eval(file_get_contents(ROOTDIR.'/system/site.cfg'));
+		eval('$config = '.file_get_contents(ROOTDIR.'/system/site.cfg').';');
 		foreach ($config as $key => $value) {
 			define($key, $value);
 		}
@@ -61,7 +32,7 @@ class Config {
 	}
 	
 	function write($config) {
-		file_put_contents(ROOTDIR.'/system/site.cfg', '$config = '.var_export($config, true).';');
+		file_put_contents(ROOTDIR.'/system/site.cfg', var_export($config, true));
 	}
 }
 
